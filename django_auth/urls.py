f70f7b217057ff9369ee4613640bc1f994a64630
Joke Heyndels <jo_hannah@outlook.com>
2019-04-08 17:22:51 +0200

    Check if user is authenticated before redirecting

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from accounts.views import index, logout, login
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls, name="admin"),
     url(r'^$', index, name="index"),
     url(r'^accounts/logout/$', logout, name="logout"),
     url(r'^accounts/login/$', login, name="login")
