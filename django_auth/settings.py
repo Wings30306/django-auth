b42dbed8edcec6efd4d0cc4544c11a272b09a62b
Joke Heyndels <jo_hannah@outlook.com>
2019-04-10 17:28:29 +0200

    Create custom password reset pages

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -127,3 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PORT = 587
